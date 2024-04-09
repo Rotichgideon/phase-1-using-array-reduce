98a422934a5e705020898a7c5f8a4a34398e5d30
gideon rotich <gideonrotich2001@gmail.com>
2024-04-09 23:09:29 +0300

    complete

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+
+// Code your solution here
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(function (countBatt, currentBatt) {
+
+    return countBatt += currentBatt;
+});
+
+console.log(totalBatteries)
